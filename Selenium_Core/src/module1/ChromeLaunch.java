9b8ee0da7fc6ac1b689f0bb5cd88cc860dd3c257
Ramakrishna <Ramakrishna@DESKTOP-MSA2E6G>
2019-05-05 23:25:50 +0530

    Chrome Launch

---
+++ Selenium_Core/src/module1/ChromeLaunch.java
@@ -0,0 +1,19 @@
+package module1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ChromeLaunch {
+
+	@Test
+	public void chrome() {
+
+		System.setProperty("webdriver.chrome.driver",
+				"E:\\JAVA\\Eclipse_Selenium_Driver\\Chrome_Driver\\New\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.toolsqa.com/selenium-tutorial/");
+	}
+
+}
